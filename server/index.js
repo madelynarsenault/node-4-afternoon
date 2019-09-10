dd74af5593e8527082a5e9f26f5c7b5e57caa8a8
Madelyn Arsenault <madelynarsenault@Madelyns-MacBook-Air-2.local>
2019-09-10 16:59:37 -0500

    finals

---
+++ server/index.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session")
+const checkForSession = require("./middlewares/checkForSession");
+const swagController = require('./controllers/swagController');
+const authController = require('./controllers/authController');
+const cartController = require('./controllers/cartController');
+
+const app = express();
+
+let {SERVER_PORT, SESSION_SECRET} = process.env
+
+app.use(express.json())
+app.use(
+    session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+})
+);
+app.use(checkForSession)
+
+
+app.post('/api/login', authController.login);
+app.post('/api/register', authController.register);
+app.post('/api/signout', authController.signout);
+app.get('/api/user', authController.getUser);
+app.get("/api/swag", swagController.read);
+app.post("/api/cart/:id", cartController.add);
+app.post("/api/cart/checkout", cartController.checkout);
+app.delete("/api/cart/:id", cartController.delete);
+
+
+
+
+app.listen(SERVER_PORT, () => {
+    console.log(`being listened to on port ${SERVER_PORT}`)
+});
+
+
+
+
+
+
+
